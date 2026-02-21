709bc6ea7935737e219409b3cca5c2d77b54bc62
benete21 <benyubun@gmail.com>
2026-02-21 22:12:51 +0200

    adding aimer

--- Assets/Settings/Scrpits/Player_01_Controls.cs
+++ Assets/Settings/Scrpits/Player_01_Controls.cs
@@ -7,24 +7,37 @@ public class Player_01_Controls : MonoBehaviour
 
     [Header("Movment")]
 
-
-
-
     public GameObject Player_01_Soul;
     public GameObject P1_firePoint;
     [SerializeField] private float Shoot_power_P_01;
 
-    private float shootInput;
-    private Vector2 moveInput;
+
+    [Header("Aim")]
+    public GameObject aimArrow;
+    public float arrowDistance = 1f;
+    public float deadzone = 0.2f;
+
+    private Vector2 aimInput;
+    private Vector2 lastAimDirection = Vector2.right;
 
     private void Update()
     {
+        if (aimInput.magnitude > deadzone)
+        {
+            lastAimDirection = aimInput.normalized;
+        }
 
+        // Rotate arrow
+        float angle = Mathf.Atan2(lastAimDirection.y, lastAimDirection.x) * Mathf.Rad2Deg;
+        aimArrow.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        // Position arrow in front of player
+        aimArrow.transform.position = transform.position + (Vector3)(lastAimDirection * arrowDistance);
     }
 
     public void OnAim(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        aimInput = context.ReadValue<Vector2>();
     }
 
     public void OnShoot(InputAction.CallbackContext context)
@@ -39,4 +52,5 @@ public class Player_01_Controls : MonoBehaviour
 
     }
 }
+
 
