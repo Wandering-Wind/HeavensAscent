699a87b10b276c4eca13cfbb17c39ec1317f998e
benete21 <benyubun@gmail.com>
2026-02-24 11:17:28 +0200

    Adding second scprot

---
+++ Assets/Settings/Scrpits/Player_02_Controls.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Player_02_Controls : MonoBehaviour
+{
+
+    [Header("Shoot")]
+
+    public GameObject Player_01_Soul;
+    public GameObject P1_firePoint;
+    [SerializeField] private float Shoot_power_P_01;
+
+
+    [Header("Aim")]
+    public GameObject aimArrow;
+    public float arrowDistance = 1f;
+    public float deadzone = 0.2f;
+
+    private Vector2 aimInput;
+    private Vector2 lastAimDirection = Vector2.right;
+    private GameObject bullet_P_01;
+
+
+    private void Update()
+    {
+        if (aimInput.magnitude > deadzone)
+        {
+            lastAimDirection = aimInput.normalized;
+        }
+
+        // Rotate arrow
+        float angle = Mathf.Atan2(lastAimDirection.y, lastAimDirection.x) * Mathf.Rad2Deg;
+        aimArrow.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        // Position arrow in front of player
+        aimArrow.transform.position = transform.position + (Vector3)(lastAimDirection * arrowDistance);
+    }
+
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        aimInput = context.ReadValue<Vector2>();
+    }
+    public void OnTeleport(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        Teleport();
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        Shoot();
+    }
+    public void Shoot()
+    {
+        if (bullet_P_01 != null)
+        {
+            return;
+        }
+        bullet_P_01 = Instantiate(Player_01_Soul, P1_firePoint.transform.position, Quaternion.identity);
+        Rigidbody2D rb = bullet_P_01.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = lastAimDirection * Shoot_power_P_01;
+
+    }
+    public void Teleport()
+    {
+        print("Asdndfsfljgb");
+        if (bullet_P_01 != null)
+        {
+            transform.position = bullet_P_01.transform.position;
+            Destroy(bullet_P_01);
+            bullet_P_01 = null;
+        }
+    }
+}
+
+
