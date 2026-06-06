096df16544acb5bfbb242d2f5dfdbd0bc824a150
Truth2004 <nhlanzekomwandla@gmail.com>
2026-06-07 00:01:17 +0200

    Updated cloudscroller
    Updated cloudscroller script and updated Game View 3 clouds with layers that have varying speeds

--- Assets/Scripts/CloudScroller.cs
+++ Assets/Scripts/CloudScroller.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 public class CloudScroller : MonoBehaviour
 {
     [Tooltip("How fast the background moves to the left.")]
@@ -8,6 +7,10 @@ public class CloudScroller : MonoBehaviour
 
     [Tooltip("The exact X position on the left where the cloud should teleport.")]
     public float despawnX = -10f;
+
+    [Header("Loop Settings")]
+    [Tooltip("How many total cloud sprites make up this scrolling line?")]
+    public int totalClouds = 4; // Set this to however many clouds you have!
 
     private float spriteWidth;
 
@@ -25,8 +28,8 @@ public class CloudScroller : MonoBehaviour
         // If the cloud crosses the invisible despawn line on the left
         if (transform.position.x <= despawnX)
         {
-            // Snap it precisely to the back of the 3-piece line on the right
-            transform.position = new Vector3(transform.position.x + (spriteWidth * 3f), transform.position.y, transform.position.z);
+            // Snap it precisely to the back of the line based on the total number of clouds
+            transform.position = new Vector3(transform.position.x + (spriteWidth * totalClouds), transform.position.y, transform.position.z);
         }
     }
 }
