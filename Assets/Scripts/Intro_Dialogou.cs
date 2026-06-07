785744eca4e1255ff655a108dbc9ee6284b94a3d
benete21 <benyubun@gmail.com>
2026-06-07 18:57:52 +0200

    dialogue system working

--- Assets/Scripts/Intro_Dialogou.cs
+++ Assets/Scripts/Intro_Dialogou.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Intro_Dialogou : MonoBehaviour
 {
-
     void Start()
     {
-        NPC_Dialogoue.Instance.StartDialogue( new string[] {"To think i would find some lost souls here.", "Hmmm I'll only permit one of you through to Heaven.", "Good Luck!"});
-        Destroy(gameObject);
+        if (NPC_Dialogoue.Instance == null)
+        {
+            Debug.LogError("NPC_Dialogoue Instance is NULL");
+            return;
+        }
+
+        NPC_Dialogoue.Instance.StartDialogue(new string[]
+        {
+        "To think I would find some lost souls here.",
+        "I'll only permit one of you through to Heaven.",
+        "Good luck!"
+        });
     }
 }
 
