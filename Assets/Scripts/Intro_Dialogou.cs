b69ffcc7767966232d4838b02f6e3874d9354959
Truth2004 <nhlanzekomwandla@gmail.com>
2026-06-08 17:57:09 +0200

    Added UI features and dialogue

--- Assets/Scripts/Intro_Dialogou.cs
+++ Assets/Scripts/Intro_Dialogou.cs
@@ -6,10 +6,16 @@ public class Intro_Dialogou : MonoBehaviour
     {
         NPC_Dialogoue.Instance.StartDialogue(new Dialogou_Line[]
         {
-            new Dialogou_Line { speakerID = "Angel", text = "You should not be here..." },
-            new Dialogou_Line { speakerID = "Angel", text = "Only one soul may pass." },
-            new Dialogou_Line { speakerID = "Devil", text = "HaHaHa Let's see which soul is mightier" },
-            new Dialogou_Line { speakerID = "Devil", text = "Let’s see who is worthy." }
+            new Dialogou_Line { speakerID = "Angel", text = "Receive a fraction of my divine spark, tiny creation. Do try not to splatter it all over the masonry." },
+            new Dialogou_Line { speakerID = "Angel", text = "Embody my purity, mortal. Or fall, and let your soul feed the soil. It makes no difference to me." },
+            new Dialogou_Line { speakerID = "Angel", text = "A drop of my grace should suffice for creatures of your... limited stature. Do not waste it." },
+            new Dialogou_Line { speakerID = "Angel", text = "Look at how they tremble. Fight well, little things, the promised land awaits... or whatever it is we told you." },
+            new Dialogou_Line { speakerID = "Angel", text = "Try to keep the blood off your robes. It ruins the aesthetic of the ascension." },
+            new Dialogou_Line { speakerID = "Devil", text = "Here's a taste of real power. Try not to choke on it, meat sack." },
+            new Dialogou_Line { speakerID = "Devil", text = "Make it bloody, make it fast, and don't make me look bad. I have a lot riding on this."},
+            new Dialogou_Line { speakerID = "Devil", text = "I'm giving you a shot at paradise, mortal. Bore me, and I'll drag you down to the pits myself just for a laugh."},
+            new Dialogou_Line { speakerID = "Devil", text = "Go on then, start hitting each other. I didn’t descend all the way down here to watch you stand around."},
+            new Dialogou_Line { speakerID = "Devil", text = "Let's see if that pathetic spark of yours can actually catch fire today."},
         });
     }
 }
